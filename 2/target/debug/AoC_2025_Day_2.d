574a7e6055e6fece466453537c5eb8618b658778
Nadelio <carefulsniffle@gmail.com>
2025-12-03 17:24:33 -0600

    Removed unnecessary files

--- 2/target/debug/AoC_2025_Day_2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\SENGa\AoC\ 2025\2\target\debug\AoC_2025_Day_2.exe: C:\Users\SENGa\AoC\ 2025\2\src\main.rs
--- 2/target/debug/deps/AoC_2025_Day_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\SENGa\AoC 2025\2\target\debug\deps\AoC_2025_Day_2.d: src\main.rs
-
-C:\Users\SENGa\AoC 2025\2\target\debug\deps\AoC_2025_Day_2.exe: src\main.rs
-
-src\main.rs:
