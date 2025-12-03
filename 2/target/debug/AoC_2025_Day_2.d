358bf1fa620a7c71dde609aa1a2423d730ecf6c3
Nadelio <carefulsniffle@gmail.com>
2025-12-03 17:22:18 -0600

    Started day 2

---
+++ 2/target/debug/AoC_2025_Day_2.d
@@ -0,0 +1,1 @@
+C:\Users\SENGa\AoC\ 2025\2\target\debug\AoC_2025_Day_2.exe: C:\Users\SENGa\AoC\ 2025\2\src\main.rs
---
+++ 2/target/debug/deps/AoC_2025_Day_2.d
@@ -0,0 +1,5 @@
+C:\Users\SENGa\AoC 2025\2\target\debug\deps\AoC_2025_Day_2.d: src\main.rs
+
+C:\Users\SENGa\AoC 2025\2\target\debug\deps\AoC_2025_Day_2.exe: src\main.rs
+
+src\main.rs:
